150d8f8d406037936171d37554375c8134b3486f
Houzeo <houzeo@Houzeos-MacBook-Air.local>
2025-01-15 16:26:03 +0530

    created houzeo_current_structure

---
+++ houzeo_current_structure/__brick__/{{file_type.snakeCase()}}s/{{#else}}/{{file_location.snakeCase()}}/{{name.snakeCase()}}_{{file_type.snakeCase()}}/{{name.snakeCase()}}_{{file_type.snakeCase()}}.dart
@@ -0,0 +1,29 @@
+{{#if (eq file_type "widget")}}
+import 'package:flutter/material.dart';
+
+class {{name.pascalCase()}}Widget extends StatelessWidget {
+  const {{name.pascalCase()}}Widget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('{{name.pascalCase()}} Widget'),
+    );
+  }
+}
+{{/if}}
+
+{{#if (eq file_type "controller")}}
+class {{name.pascalCase()}}Controller {
+}
+{{/if}}
+
+{{#if (eq file_type "model")}}
+class {{name.pascalCase()}}Model {
+}
+{{/if}}
+
+{{#if (eq file_type "repo")}}
+class {{name.pascalCase()}}Repository {
+}
+{{/if}}
